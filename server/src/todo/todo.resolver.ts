802bed460c8c2729ddaea3f351a4a423bb04022c
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-03-15 12:04:40 -0300

    createTodo mutation

--- server/src/todo/todo.resolver.ts
+++ server/src/todo/todo.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Args } from "@nestjs/graphql";
+import { Resolver, Query, Args, Mutation } from "@nestjs/graphql";
 import { TodoService } from "./todo.service";
 import { Todo } from "../graphql.generated";
 
@@ -8,17 +8,11 @@ export class TodoResolver {
 
     @Query()
     async todo(@Args("id") id: number): Promise<Todo | null> {
-        const todo = await this.todoService.findOneById(id);
-        if (!todo) {
-            return null;
-        }
-        return {
-            ...todo,
-            createdAt: todo.createdAt.toISOString() ?? undefined,
-            updatedAt: todo.updatedAt.toISOString() ?? undefined,
-            completedAt: todo.completedAt?.toISOString() ?? undefined,
-            completed: !!todo.completedAt,
-            description: todo.description ?? undefined,
-        };
+        return await this.todoService.findOneById(id);
+    }
+
+    @Mutation("createTodo")
+    async createTodo(@Args("description") description: string): Promise<Todo> {
+        return await this.todoService.createTodo({ description });
     }
 }
--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaClient, Todo } from "@prisma/client";
+import { Todo as DomainTodo } from "../graphql.generated";
 const prisma = new PrismaClient();
 
 @Injectable()
 export class TodoService {
-    async findOneById(id: number): Promise<Todo | null> {
-        return await prisma.todo.findOne({
+    async findOneById(id: number): Promise<DomainTodo | null> {
+        const todo = await prisma.todo.findOne({
             where: {
                 id,
             },
         });
+        if (!todo) {
+            return null;
+        }
+        return entityToDomain(todo);
+    }
+
+    async createTodo(props: { description: string }) {
+        const todo = await prisma.todo.create({
+            data: {
+                description: props.description,
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString(),
+            },
+        });
+        return entityToDomain(todo);
     }
 }
+
+const entityToDomain = (entity: Todo): DomainTodo => ({
+    ...entity,
+    createdAt: entity.createdAt.toISOString() ?? undefined,
+    updatedAt: entity.updatedAt.toISOString() ?? undefined,
+    completedAt: entity.completedAt?.toISOString() ?? undefined,
+    completed: !!entity.completedAt,
+    description: entity.description ?? undefined,
+});
